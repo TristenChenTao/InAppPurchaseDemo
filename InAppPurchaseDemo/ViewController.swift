63344f2e0229e5590aef3b5e0c7c428df3c208cc
Tristen陈涛 <TristenChenTao@gmail.com>
2017-07-19 11:11:14 +0800

    Initial Commit

---
+++ InAppPurchaseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InAppPurchaseDemo
+//
+//  Created by Tristen陈涛 on 2017/7/19.
+//  Copyright © 2017年 Tristen陈涛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
